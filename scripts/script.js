d6741e5e8aac3a57e6008a0c12669b7eeb174e60
Marina <zvyagintsevame@yandex.ru>
2021-09-28 14:43:51 +0200

    feat: add-form render

--- scripts/script.js
+++ scripts/script.js
@@ -1,43 +1,68 @@
 /* Модальное окно и форма */
-const modal = document.querySelector(".modal");
-const form = document.querySelector(".form");
+const modalEdit = document.querySelector(".modal_edit-info");
+const modalAdd = document.querySelector(".modal_add-place");
+const forms = document.querySelectorAll(".form");
 
 /* Кнопки */
-const editButton = document.querySelector(".profile__edit-button");
-const closeButton = document.querySelector(".form__close-button");
+const closeButtons = document.querySelectorAll(".form__close-button");
 
 /* Инпуты */
 const nameInput = document.querySelector(".form__input_value_name");
 const jobInput = document.querySelector(".form__input_value_job");
+const placeInput = document.querySelector(".form__input_value_place");
+const linkInput = document.querySelector(".form__input_value_link");
 
 /* Значения инпутов */
 const nickname = document.querySelector(".profile__title");
 const job = document.querySelector(".profile__subtitle");
 
 /* Открыть модальное окно */
-function openModal() {
-    modal.classList.add("modal_open");
+function openModal(event) {
+    if (event.target.closest('.profile__edit-button')) {
+        modalEdit.classList.add("modal_open");
 
-    nameInput.value = nickname.textContent;
-    jobInput.value = job.textContent;
+        nameInput.value = nickname.textContent;
+        jobInput.value = job.textContent;
+    }
+
+    if (event.target.closest('.profile__add-button')) {
+        modalAdd.classList.add("modal_open");
+    }
 }
 
 /* Закрыть модальное окно */
 function closeModal(event) {
-    modal.classList.remove("modal_open");
+    if (event.target.closest('.modal_edit-info')) {
+        modalEdit.classList.remove("modal_open");
+    }
+
+    if (event.target.closest('.modal_add-place')) {
+        modalAdd.classList.remove("modal_open");
+    }
 }
 
 /* Сохранить информацию */
 function saveInfo(event) {
-    event.preventDefault();
+    if (event.target.closest('.modal_edit-info')) {
+        event.preventDefault();
 
-    nickname.textContent = nameInput.value;
-    job.textContent = jobInput.value;
+        nickname.textContent = nameInput.value;
+        job.textContent = jobInput.value;
+    }
+
+    if (event.target.closest('.modal_add-place')) {
+        event.preventDefault();
+    }
 
     closeModal(event);
 }
 
-/* Слушатели событий */
-editButton.addEventListener("click", openModal);
-closeButton.addEventListener("click", closeModal);
-form.addEventListener("submit", saveInfo);
+closeButtons.forEach(item => {
+    item.addEventListener("click", closeModal);
+});
+
+forms.forEach(item => {
+    item.addEventListener("submit", saveInfo);
+});
+
+document.addEventListener('click', openModal);
