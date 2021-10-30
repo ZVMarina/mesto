4a7366c607a90663fe7f8ae0bbf630d1a71c190a
Marina <zvyagintsevame@yandex.ru>
2021-10-30 09:11:33 +0200

    refactor: class card

---
+++ scripts/Card.js
@@ -0,0 +1,52 @@
+export default class Card {
+    constructor(data, templateSelector) {
+        this._name = data.name;
+        this._link = data.link;
+        this._templateSelector = templateSelector;
+    }
+
+    _getTemplate() {
+        const cardElement = document
+            .querySelector(this._templateSelector)
+            .content
+            .querySelector('.card')
+            .cloneNode(true);
+
+        return cardElement;
+    }
+
+    generateCard() {
+        this._element = this._getTemplate();
+        this._setEventListeners();
+
+        /* Image */
+        const cardImage = this._element.querySelector('.card__image');
+        cardImage.src = this._link;
+        cardImage.alt = this._name;
+
+        /* Title */
+        this._element.querySelector('.card__title').textContent = this._name;
+
+        return this._element;
+    }
+
+    _setEventListeners() {
+        /* Like */
+        this._element.querySelector('.card__button_type_like').addEventListener('click', () => {
+            this._toggleActiveLike();
+        });
+
+        /* Delete card */
+        this._element.querySelector('.card__button_type_delete').addEventListener('click', () => {
+            this._removeCard();
+        });
+    }
+
+    _toggleActiveLike() {
+        this._element.querySelector('.card__button_type_like').classList.toggle('card__button_active');
+    }
+
+    _removeCard() {
+        this._element.querySelector('.card__button_type_delete').closest('.card').remove();
+    }
+}
