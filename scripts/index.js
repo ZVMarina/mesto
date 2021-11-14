be956172d0ec20832289d5920a83d5d109d5a38c
Marina <zvyagintsevame@yandex.ru>
2021-11-14 11:30:08 +0200

    ref

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,6 @@
 import Card from './Card.js';
 import FormValidator from './FormValidator.js';
+import Section from './Section.js';
 import {validationConfig, initialCards} from './data.js';
 
 /* Карточки */
